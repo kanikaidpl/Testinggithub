07d3668fb94299e79785311c8ca125aa651f0968
Kanika <Kanika@LAPTOP-BHBANM5F>
2022-04-08 15:29:45 +0530

    first commit

---
+++ git/src/main/sources/Testgitg/git/testrest.java
@@ -0,0 +1,10 @@
+package Testgitg.git;
+
+public class testrest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ git/src/test/sources/Testgitg/git/AppSpringConfig.java
@@ -0,0 +1,14 @@
+package Testgitg.git;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Base configuration for spring
+ */
+@Configuration
+@ComponentScan("Testgitg.git")
+public class AppSpringConfig
+{
+}
